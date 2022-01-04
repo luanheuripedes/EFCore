d2a2a59ef2fa2187ab11aa5c5888f691f37e0571
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2022-01-03 22:25:20 -0300

    Batching

--- Batching/Program.cs
+++ Batching/Program.cs
@@ -1,78 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
 
-namespace EFCoreDemo // Note: actual namespace depends on the project name.
+namespace Batching
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            
-            using(var db = new LivroContexto()){
+            using (var db = new LivrosContext())
+            {
+                db.Database.EnsureDeleted(); // deleta a database caso ela exista
+                db.Database.EnsureCreated(); // caso exista alguma linha na tabela ele deleta
 
-                //Faz com que eu crie o banco de dados sem migration
-                //db.Database.EnsureCreated();
+                if (db.Livros.Any())
+                {
+                    db.Database.ExecuteSqlRawAsync("DELETE FROM dbo.Livros");
+                }
 
-                //Migrations primeiro ver como modelou a tabela para depois transmitir e criar o banco previamente
-                //Livro l1 = new Livro("Teste", "Marquinho", 1979);
-                //Livro l2 = new Livro("Escolha", "Joaquim", 1999);
+                db.Livros.Add(new Livro { Titulo = "Domain-Driven Design: Tackling Complexity in the Heart of Software", Autor = "Eric Evans", AnoPublicacao = 2003 });
+                db.Livros.Add(new Livro { Titulo = "Agile Principles, Patterns, and Practices in C#", Autor = "Robert C. Martin", AnoPublicacao = 2006 });
+                db.Livros.Add(new Livro { Titulo = "Clean Code: A Handbook of Agile Software Craftsmanship", Autor = "Robert C. Martin", AnoPublicacao = 2008 });
+                db.Livros.Add(new Livro { Titulo = "Implementing Domain-Driven Design", Autor = "Vaughn Vernon", AnoPublicacao = 2013 });
+                db.Livros.Add(new Livro { Titulo = "Patterns, Principles, and Practices of Domain-Driven Design", Autor = "Scott Millet", AnoPublicacao = 2015 });
+                db.Livros.Add(new Livro { Titulo = "Refactoring: Improving the Design of Existing Code ", Autor = "Martin Fowler", AnoPublicacao = 2012 });
 
-                //Adicionar os livros na propriedade no contexto do db
-                //db.Livros.Add(new Livro {Titulo = "Teste", Autor = "Marquinho", AnoPublicacao = 1979});
-                //db.Livros.Add(new Livro {Titulo = "Escolha", Autor = "Joaquim", AnoPublicacao = 1999});
-
-                //Salva no banco os dados
-                //db.SaveChanges();
-
-
-                //Consulta dos dados que eu inseridos
-                System.Console.WriteLine(  " ************** Resultados *********");
-                //Traz tudo da tabela livro do banco
-                db.Livros.ForEachAsync(x => Console.WriteLine($"Título: {x.Titulo} | Autor: {x.Autor}"));
+                db.SaveChanges(); // envia as informações para o banco
             }
-
-
-
-
-        }
-    }
-
-    //Contexto para conversar com o banco -- configuração do EF
-    public class LivroContexto : DbContext{
-        
-        //faz com manipulemos os dados na nossa tabela --consultas - inserir dados
-        public DbSet<Livro> Livros { get; set; }
-
-        //configuração e modelagem dos dados que vao ser refletidos no banco
-        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder){
-            optionBuilder
-            .UseSqlServer(@"Server=localhost;Database=EFCoreDemo;User Id=sa;Password=yourStrong(!)Password;");
         }
 
-        //Metodo de modelagem da opçoes do bd //Configurar e Modelar a tabela na hora de criar no banco
-        protected override void OnModelCreating(ModelBuilder modelBuilder){
-            modelBuilder.Entity<Livro>().ToTable("Livro");
+        public class LivrosContext : DbContext
+        {
+            public DbSet<Livro> Livros { get; set; }
 
-            //Definindo a chave primaria da tabela
-            modelBuilder.Entity<Livro>().HasKey(p => p.LivroId);
-
-            //Adiciona um tipo a coluna
-            modelBuilder.Entity<Livro>().Property(p => p.Titulo).HasColumnType("varchar(50)");
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(
+                    @"Server=localhost;Database=EFCoreDemo;User Id=sa;Password=yourStrong(!)Password;",
+                    options => options.MaxBatchSize(2));
+            }
         }
 
-
-    }
-
-    public class Livro{
-
-        
-        public int LivroId { get; set; }
-        public string Titulo { get; set; }
-        public string Autor { get; set; }
-        public int AnoPublicacao { get; set; }
-
+        public class Livro
+        {
+            public int LivroId { get; set; }
+            public string Titulo { get; set; }
+            public string Autor { get; set; }
+            public int AnoPublicacao { get; set; }
+        }
     }
 }
